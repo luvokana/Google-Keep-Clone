cc98bc198a3b843ddbf8ebaa3ef7e6544b253c50
Luvo <luvo132@gmail.com>
2026-06-07 20:47:05 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,88 @@
+class Note {
+  constructor(id, title, text) {
+    this.id = id;
+    this.title = title;
+    this.text = text;
+  }
+}
+
+class App {
+  constructor() {
+    this.notes = [];
+
+    this.$activeForm = document.querySelector(".active-form");
+    this.$inactiveForm = document.querySelector(".inactive-form");
+    this.$noteTitle = document.querySelector("#note-title");
+    this.$noteText = document.querySelector("#note-text");
+
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    document.body.addEventListener("click", (event) => {
+      this.handleFormClick(event);
+    });
+  }
+
+  handleFormClick(event) {
+    const isActiveFormClickedOn = this.$activeForm.contains(event.target);
+    const isInactiveFormClickedOn = this.$inactiveForm.contains(event.target);
+    const title = this.$noteTitle.value;
+    const text = this.$noteText.value;
+
+    if (isInactiveFormClickedOn) {
+      this.openActiveForm();
+    } else if (!isInactiveFormClickedOn && !isActiveFormClickedOn) {
+      this.addNote({ title, text });
+      this.closeActiveForm();
+    }
+  }
+
+  openActiveForm() {
+    this.$inactiveForm.style.display = "none";
+    this.$activeForm.style.display = "block";
+    this.$noteText.focus();
+  }
+
+  closeActiveForm() {
+    this.$inactiveForm.style.display = "block";
+    this.$activeForm.style.display = "none";
+    this.$noteText.value = "";
+    this.$noteTitle.value = "";
+  }
+
+  addNote({ title, text }) {
+    if(text != "") {
+      const newNote = new Note(cuid(), title, text);
+      this.notes = [...this.notes, newNote];
+      this.displayNotes();
+    }
+    
+  }
+
+  editNote(id, { title, text }) {
+    this.notes = this.notes.map((note) => {
+      if (note.id == id) {
+        note.title = title;
+        note.text = text;
+      }
+      return note;
+    });
+  }
+
+  displayNotes() {
+    this.notes.map((note) =>
+      console.log(`
+        ID: ${note.id}
+        Title: ${note.title}
+        Text: ${note.text}
+        `)
+    );
+  }
+
+  deleteNote(id) {
+    this.notes = this.notes.filter((note) => note.id != id);
+  }
+}
+
+const app = new App();
