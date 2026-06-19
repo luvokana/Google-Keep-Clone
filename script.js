d5444b9f8d84ba60fa451dd8f3d11c889bb208b6
Luvo <luvo132@gmail.com>
2026-06-19 19:06:39 +0200

    Continue work

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ class App {
     this.$inactiveForm = document.querySelector(".inactive-form");
     this.$noteTitle = document.querySelector("#note-title");
     this.$noteText = document.querySelector("#note-text");
+    this.$noteText = document.querySelector("#note-text");
     this.$notes = document.querySelector(".notes");
 
     this.addEventListeners();
@@ -69,20 +70,69 @@ class App {
       }
       return note;
     });
+     this.displayNotes();
   }
 
   displayNotes() {
-    this.$notes.innerHTML = this.notes.map((note) =>
-    `
-        ID: ${note.id}
-        Title: ${note.title}
-        Text: ${note.text}
-        `)
+    this.$notes.innerHTML = this.notes.map(
+      (note) =>
+        `
+          <div class="note" id="${note.id}">
+            <span class="material-symbols-outlined check-circle"
+              >check_circle</span
+            >
+            <div class="title">${note.title}</div>
+            <div class="text">${note.text}</div>
+            <div class="note-footer">
+              <div class="tooltip">
+                <span class="material-symbols-outlined hover small-icon"
+                  >add_alert</span
+                >
+                <span class="tooltip-text">Remind me</span>
+              </div>
+              <div class="tooltip">
+                <span class="material-symbols-outlined hover small-icon"
+                  >person_add</span
+                >
+                <span class="tooltip-text">Collaborator</span>
+              </div>
+              <div class="tooltip">
+                <span class="material-symbols-outlined hover small-icon"
+                  >palette</span
+                >
+                <span class="tooltip-text">Change Color</span>
+              </div>
+              <div class="tooltip">
+                <span class="material-symbols-outlined hover small-icon"
+                  >image</span
+                >
+                <span class="tooltip-text">Add Image</span>
+              </div>
+              <div class="tooltip">
+                <span class="material-symbols-outlined hover small-icon"
+                  >archive</span
+                >
+                <span class="tooltip-text">Archive</span>
+              </div>
+              <div class="tooltip">
+                <span class="material-symbols-outlined hover small-icon"
+                  >more_vert</span
+                >
+                <span class="tooltip-text">More</span>
+              </div>
+            </div>
+          </div>
+        `
+    ).join("");
+    
   }
+ 
+
 
   deleteNote(id) {
     this.notes = this.notes.filter((note) => note.id != id);
+    this.displayNotes();
   }
 }
-
 const app = new App();
+
